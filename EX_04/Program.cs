210c57fca137e834171fd49168651045a60000a3
DimaSannikov <dimasannikov@gmail.com>
2023-03-07 01:00:29 +0500

    added EX_04

---
+++ EX_04/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+
+
+void ReturnElements()
+{
+    int[] array = new int[8] {0, 0, 0, 0, 0, 0, 0, 0};
+    for(int i = 0; i < 8; i++)
+    {
+    int num = new Random().Next(2);
+    array[i] = num;
+    Console.Write($"{array[i]} ");
+    }
+}
+
+ReturnElements();
