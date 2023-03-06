f45fb2354ed45505d603a9c10a7077bef4bdecae
DimaSannikov <dimasannikov@gmail.com>
2023-03-07 01:00:05 +0500

    added EX_01

---
+++ EX_01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (А)
+// и выдаёт сумму чисел от A до B.
+
+
+int GetSum(int startNumber, int endNumber)
+{
+    int sum = 0;
+    for(int i = startNumber; i <= endNumber; i++)
+    {
+        sum += i;
+    }
+    
+    return sum;
+}
+
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int sum = GetSum(1, num);
+Console.WriteLine(sum);
