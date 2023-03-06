13f693997c8546ed1fd56e144c4c56754e9e1a8b
DimaSannikov <dimasannikov@gmail.com>
2023-03-07 01:00:20 +0500

    added EX_03

---
+++ EX_03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдаёт произведение чисел от 1 до N.
+
+int GetMulti(int startNumber, int endNumber)
+{
+    int multi = 1;
+    for(int i = startNumber; i <= endNumber; i++)
+    {
+        multi *= i;
+    }
+    
+    return multi;
+}
+
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int multi = GetMulti(1, num);
+Console.WriteLine(multi);
