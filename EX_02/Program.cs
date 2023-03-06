f826c3d622a9ab00c042ceb6de7380a1f51bfe81
DimaSannikov <dimasannikov@gmail.com>
2023-03-07 01:00:14 +0500

    added EX_02

---
+++ EX_02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт количество цифр в числе.
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int GetLen(int number)
+{
+    
+    int numLen = 0;
+    int digit = number;
+    while(digit != 0)
+    {
+        digit = digit / 10;
+        numLen += 1;
+    }
+    return numLen;
+}
+
+Console.WriteLine(GetLen(num));
