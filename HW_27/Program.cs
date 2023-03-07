602d207e22701ef39c58e7e902594cd9e9ad1a9e
DimaSannikov <dimasannikov@gmail.com>
2023-03-07 11:11:36 +0500

    added HW_27

---
+++ HW_27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+
+int GetSum(int num)
+{
+    int sum = 0;
+    int newNum = num;
+    while(newNum != 0)
+    {
+        int digit = newNum % 10;
+        newNum = newNum / 10;
+        sum += digit;
+    }
+    return sum;
+}
+
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetSum(number));
