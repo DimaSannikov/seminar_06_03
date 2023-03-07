5af6c1b78f86afe87c07861a1b0d67f1e36e73c2
DimaSannikov <dimasannikov@gmail.com>
2023-03-07 11:11:29 +0500

    added HW_25

---
+++ HW_25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход два числа
+// (A и B) и возводит число A в натуральную степень B.
+
+double GetDegree(int number, int degree)
+{
+    double newNum = Math.Pow(number, degree);
+    return newNum;
+}
+
+Console.Write("Введите целое число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.Write("Введите еще одно целое число: ");
+int deg = int.Parse(Console.ReadLine());
+
+double newNum = GetDegree(num, deg);
+Console.WriteLine(newNum);
